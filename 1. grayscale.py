a1abb546561fef97002e38d7b03d2bb73ac70efc
Ilakkiya-19 <112751775+Ilakkiya-19@users.noreply.github.com>
2024-02-05 18:53:14 +0530

    Update 1. grayscale.py

--- 1. grayscale.py
+++ 1. grayscale.py
@@ -1,9 +1,9 @@
 import cv2 
-path = "E:/Computer Vision/computer vision input and output/1.grayscaleinput.png"
-img =cv2.imread(path)
-cv2.imshow("original image",img)
-cv2.waitKey(0)
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("GrayScale",imgGray)
-cv2.waitKey(0)
+image = cv2.imread("C:/Users/susri/OneDrive/Desktop/COMPUTER VISION/CV EX 1.jpg") 
+cv2.imshow('Original', image) 
+cv2.waitKey(0) 
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) 
+cv2.imshow('Grayscale', gray_image) 
+cv2.waitKey(0) 
 cv2.destroyAllWindows()
+
